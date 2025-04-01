1cd1c5681d7703c558abda85d0be827a9fc39635
Gabriel Hazi <hazigabriel@gmail.com>
2025-04-01 19:03:29 +0300

    Add lint, prettier, ant configs

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Button type="primary">Button</Button>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ src/app/AntdRegistry.tsx
@@ -0,0 +1,31 @@
+// app/AntdRegistry.tsx
+'use client';
+
+import '@ant-design/v5-patch-for-react-19';
+
+import { AntdRegistry as AntdNextJsRegistry } from '@ant-design/nextjs-registry';
+import { unstableSetRender } from 'antd';
+import { createRoot } from 'react-dom/client';
+
+
+
+// added from https://ant.design/docs/react/v5-for-19
+if (typeof window !== 'undefined') {
+    unstableSetRender((node, container) => {
+        container._reactRoot ||= createRoot(container);
+        const root = container._reactRoot;
+        root.render(node);
+        return async () => {
+            await new Promise(resolve => setTimeout(resolve, 0));
+            root.unmount();
+        };
+    });
+}
+
+export default function AntdRegistry({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <AntdNextJsRegistry>{children}</AntdNextJsRegistry>;
+}
